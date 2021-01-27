62cff3aef25021e6e9ffa58cf16976608d61c213
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2021-01-27 08:58:39 -0300

    Add eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -24,23 +25,25 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta property="og:image" content={db.bg}/>
+        <meta property="og:image" content={db.bg} />
         <meta property="og:title" content="Alura Quiz Pedro" key="title" />
-        <meta property='og:description' content='Projeto feito durante a imersão React + NextJs da Alura'/>
-        <meta property='og:url' content='https://aluraquiz-base.pmantovani.vercel.app/'/>
+        <meta property="og:description" content="Projeto feito durante a imersão React + NextJs da Alura" />
+        <meta property="og:url" content="https://aluraquiz-base.pmantovani.vercel.app/" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
